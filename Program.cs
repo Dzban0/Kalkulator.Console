bcc72906541917ed83049f935731a3878f75c68f
MrKazio <115210775+Dzban0@users.noreply.github.com>
2023-05-23 19:55:04 +0200

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Kalkulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string start = "KALKULATOR";
+                Console.WriteLine(start);
+
+                while(true)
+                {
+                    Console.WriteLine("I liczba");
+                    var number1 = GetInput();
+
+                    Console.WriteLine("OPERACJA. Możliwe: '+', '-', '*', '/',");
+                    var operation = Console.ReadLine();
+
+                    Console.WriteLine("II liczba");
+                    var number2 = GetInput();
+
+                    var x = Calculate(number1, number2, operation);
+                    Console.WriteLine($"Wynik: {x}.");
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+            static double GetInput()
+            {
+                if (!double.TryParse(Console.ReadLine(), out double input))
+                    throw new Exception("Podana wartość nie jest liczbą.");
+
+                return input;
+            }
+
+            static double Calculate(double number1, double number2, string operation) 
+            {
+                switch (operation)
+                {
+                    case "+":
+                        return number1 + number2;
+                    case "-":
+                        return number1 - number2;
+                    case "*":
+                        return number1 * number2;
+                    case "/":
+                        return number1 / number2;
+                    default:
+                        throw new Exception("ZŁA OPERACJA!!!");
+                }
+            }
+        }
+    }
+}
